fadab2ed6b9298a20ce0e1cf0a3d0427e8c9a88e
Colin Choi <cechoi@cs.ualberta.ca>
2020-01-06 18:08:14 -0700

    Lab1

---
+++ Lab1/script.py
@@ -0,0 +1,7 @@
+import requests
+
+#print(requests.__version__)
+
+#var = requests.get("https://raw.githubusercontent.com/colinchoix/404Labs/master/Lab1/script.py")
+
+print(requests.get("http://www.google.com/"))
